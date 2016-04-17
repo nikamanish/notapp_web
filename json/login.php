4a2fe9d9132752c48a78b25a3f45efbc69ceb368
Manish Nikam <Manish Nikam>
2016-04-18 03:03:02 +0530

    Class and Branch sent with login info.

--- json/login.php
+++ json/login.php
@@ -23,10 +23,26 @@
         $t = mysqli_fetch_assoc($re);
         $prefs = $t['prefs'];
         
+        $sql = "select D.d_name from department D inner join student S on D.d_id = S.d_id where S.u_id='$u_id'";
+        $branch_res=mysqli_query($conn,$sql) or die(mysqli_error($conn));
+        $branch_res_arr = mysqli_fetch_assoc($branch_res);
+        $branch = $branch_res_arr['d_name'];
+        
+        $sql = "select C.c_name from class C inner join student S on C.c_id = S.c_id where S.u_id='$u_id'";
+        $class_res=mysqli_query($conn,$sql) or die(mysqli_error($conn));
+        $class_res_arr = mysqli_fetch_assoc($class_res);
+        $class = $class_res_arr['c_name'];
+        
+        $branch = strtolower($branch);
+        
+        
         if($password == $arr['pword'])
         {
             $arr['consent'] = 1;
             $arr['dprefs'] = $prefs;
+            $arr['d_name'] = $branch;
+            $arr['c_name'] = $class;
+            
             $rows['result'][] = $arr;
             echo json_encode($rows);        
         }
